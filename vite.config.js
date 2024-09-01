7c4bbb1d8e89e17c12505689156da185964a8b5d
queenyxies <queenycasaclang@gmail.com>
2024-09-01 18:43:57 +0800

    Fix deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // Customize the port if needed
+  },
+  build: {
+    outDir: 'dist', // Customize the output directory if needed
+  },
+});
