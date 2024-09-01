32c67521e668d6556fca8af83178544ba52dc385
queenyxies <queenycasaclang@gmail.com>
2024-09-01 18:53:16 +0800

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // Customize the port if needed
-  },
-  build: {
-    outDir: 'dist', // Customize the output directory if needed
-  },
+  base: "/Charik-Frontend-Interview---Mary-Queen-Casaclang/",
 });
